bd5b22ff54c0c5e29f19156e0456decb88745864
mtthw-s <mtthwsanford@gmail.com>
2018-05-28 19:21:55 -0700

    Working application with links to applications

--- public/app.js
+++ public/app.js
@@ -4,10 +4,31 @@ var PortfolioApp = angular.module('PortfolioApp', [
 ]);
     
 PortfolioApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/main', {
+    $routeProvider.when('/education', {
+        templateUrl: 'partials/education.html',
+        controller: 'MainController'
+    })
+    .when('/apps/formletter', {
+        templateUrl: 'partials/apps/SimpleFormLetter.html',
+        controller: 'MainController'
+    })
+    .when('/apps/craps', {
+        templateUrl: 'partials/apps/craps.html',
+        controller: 'MainController'
+    })
+    .when('/apps/movie', {
+        templateUrl: 'partials/apps/Movie.html',
+        controller: 'MainController'
+    })
+    .when('/apps/change', {
+        templateUrl: 'partials/apps/ChangeTemplate.html',
+        controller: 'MainController'
+    })
+    .when('/', {
         templateUrl: 'partials/main.html',
         controller: 'MainController'
-    }).otherwise({
-        redirectTo: '/list'
+    })
+    .otherwise({
+        redirectTo: '/'
     });
 }]);
